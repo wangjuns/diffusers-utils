f8c3b250c1e1e0b0c271d76a4c2881dbdea2a26e
wangjun <jun.wangj@me.com>
2023-03-10 23:45:40 +0800

    add crop_img

--- diffusers_utils/images_util.py
+++ diffusers_utils/images_util.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import cv2
+import numpy
 
 
 def image_grid(imgs: list[Image.Image], rows: int, cols: int) -> Image.Image:
@@ -9,3 +11,37 @@ def image_grid(imgs: list[Image.Image], rows: int, cols: int) -> Image.Image:
     for i, img in enumerate(imgs):
         grid.paste(img, box=(i % cols * w, i // cols * h))
     return grid
+
+
+def crop_img(img: str | Image.Image, target_size: tuple[int, int] = (512, 512)):
+    if isinstance(img, str):
+        img = cv2.imread(img)  # 读取图片
+    else:
+        img = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
+
+    (height, width, _) = img.shape
+
+    t_img_len = height if height < width else width
+
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')  # 加载人脸检测模型
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 转换为灰度图
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # 检测人脸
+    if len(faces) == 0:
+        print(f"no face detected in image {img}")
+        return
+    (x, y, w, h) = faces[0]
+    if height < width:
+        y = 0
+        h = height
+        x = max(0, (x + w//2) - height // 2)
+        w = height
+    else:
+        x = 0
+        w = width
+        y = max(0, (y + h // 2) - width // 2)
+        h = width
+
+    crop_img = img[y:y+h, x:x+w]  # 裁剪人脸区域
+    crop_img = cv2.resize(crop_img, target_size)  # 将人脸缩放为正方形
+    return crop_img
--- test/test_images_util.py
+++ test/test_images_util.py
@@ -1,5 +1,6 @@
 import unittest
 from PIL import Image
+import cv2
 
 from diffusers_utils import images_util
 
@@ -7,14 +8,20 @@ from diffusers_utils import images_util
 
 
 class TestDict(unittest.TestCase):
-    def test_init(self):
+    def test_grid(self):
         images = []
         for i in range(1, 3):
             img = Image.open(f'test/p{i}.jpeg')
             images.append(img)
 
         result = images_util.image_grid(images, 1, 2)
+
         # result.show()
+
+    def test_crop(self):
+        img = Image.open(f'test/318231715.jpeg')
+        img = images_util.crop_img(img, (512, 512))
+        cv2.imwrite("test/318231715_1.jpg", img)
 
 
 if __name__ == '__main__':
